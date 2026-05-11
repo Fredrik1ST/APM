a9b73f7265bc115443a65300328a1fec2d10c130
Fredrik Taklo <fredriktaklo@hotmail.com>
2026-05-11 16:48:33 +0200

    Add new 2-way arduino interface

--- apm/arduino_comm.py
+++ apm/arduino_comm.py
@@ -1,5 +1,147 @@
 '''
 Handling of communication between ZED Box and Arduino via Ethernet (TCP Socket)
 
+Message formats:
+ * ZED Box -> Arduino - 32 bytes:
+ *     - Byte 0.0:    Run signal          (bool)
+ *     - Byte 1.0:    Emergency brake     (bool)
+ *     - Byte 2.0:    Green LED           (bool)
+ *     - Byte 3.0:    Red LED             (bool)
+ *     - Byte 4-15:   Spare             
+ *     - Byte 16-19:  Message nr          (uint32_t)    (Increments per message. Can be used as watchdog / lost message detector.)
+ *     - Byte 20-23:  Steer angle         (float)
+ *     - Byte 24-27:  Desired speed PWM   (float)       (Pulse width in microseconds)
+ *     - Byte 28-31:  Spare
+ * 
+ * Arduino -> ZED Box - 32 bytes:
+ *     - Byte 0.0:    Run signal          (bool)        (Mirror incoming message)
+ *     - Byte 1.0:    Emergency brake     (bool)        (Mirror incoming message)
+ *     - Byte 2.0:    Green LED           (bool)        (Read pin value)
+ *     - Byte 3.0:    Red LED             (bool)        (Read pin value)
+ *     - Byte 6-15:   Spare
+ *     - Byte 16-19:  Message nr          (uint32_t)    (Increments per message)
+ *     - Byte 20-23:  ESC min PWM         (float)       (ESC calibrated min PWM limit - zero point)
+ *     - Byte 24-27:  ESC max PWM         (float)       (ESC calibrated max PWM limit)
+ *     - Byte 28-31:  PWM max speed limit (float)       (Hardcoded PWM limit for safety)
 
 '''
+
+import socket
+import struct
+import logging
+
+log = logging.getLogger(__name__)
+
+class message_commands:
+    """Formatted message of commands sent from ZED Box to Arduino"""
+    def __init__(self):
+        self.run = False
+        self.emergency_brake = False
+        self.green_led = False
+        self.red_led = False
+        self.message_nr = 0
+        self.steer_angle = 90.0
+        self.speed_pwm = 0.0
+    
+    @property
+    def packed(self):
+        """Pack the message into bytes for sending to Arduino"""
+        return struct.pack('<4?12xIff4x', 
+                           self.run, 
+                           self.emergency_brake, 
+                           self.green_led, self.red_led, 
+                           self.message_nr, 
+                           self.steer_angle, 
+                           self.speed_pwm)
+    
+class message_feedback:
+    """Formatted message of feedback sent from Arduino to ZED Box"""
+    def __init__(self):
+        self.running = False
+        self.emergency_brake = False
+        self.green_led = False
+        self.red_led = False
+        self.message_nr = 0
+        self.esc_min_pwm = 0.0
+        self.esc_max_pwm = 0.0
+        self.pwm_speed_limit = 0.0
+    
+    @property
+    def packed(self):
+        """Pack the message into bytes for sending to ZED Box"""
+        return struct.pack('<4?12xIfff', 
+                           self.running, 
+                           self.emergency_brake, 
+                           self.green_led, self.red_led, 
+                           self.message_nr, 
+                           self.esc_min_pwm, 
+                           self.esc_max_pwm, 
+                           self.pwm_speed_limit)
+
+
+class TCPServer:
+    '''
+    A simple TCP server for communicating with an Arduino (or another TCP client) via Ethernet.
+    Only one client can connect at a time.
+    '''
+
+    def __init__(self, host='192.168.56.1', port=49152):
+        self.host = host
+        self.port = port
+        self.server_socket = None
+        self.client_socket = None
+        self.client_address = None
+        self.is_connected = False
+    
+
+    def start(self):
+        """Start TCP server and wait for a client to connect."""
+        try:
+            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Allow immediate reconnections
+
+            self.server_socket.bind((self.host, self.port))
+            self.server_socket.listen(1) # Wait for a client to connect
+
+            log.info(f'Started TCP Server {self.host}:{self.port}. Waiting for connection...')
+
+            self.client_socket, self.client_address = self.server_socket.accept()
+            self.is_connected = True
+            log.info(f'Client {self.client_address} connected')
+
+            return True
+        
+        except socket.error as e:
+            log.error(f'TCP socket error: {e}')
+            return False
+        
+
+    def send(self, data):
+        """Send a byte message to the connected client."""
+        if self.is_connected and self.client_socket:
+            try:
+                self.client_socket.sendall(data)
+                log.debug(f'Bytes sent to {self.client_address}: {data.hex()}')
+                return True
+            except socket.error as e:
+                log.error(f'Error sending data: {e}')
+                return False
+    
+    def receive(self, data_length=32):
+        """Receive a byte message from the connected client."""
+        if self.is_connected and self.client_socket:
+            try:
+                data = self.client_socket.recv(data_length)
+                log.debug(f'Bytes received from {self.client_address}: {data.hex()}')
+            except socket.error as e:
+                log.error(f'Error receiving data: {e}')
+                return None
+    
+    def stop(self):
+        """Stop the server and close all sockets."""
+        if self.client_socket:
+            self.client_socket.close()
+        if self.server_socket:
+            self.server_socket.close()
+        self.is_connected = False
+        log.info('TCP Server stopped')
--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,63 +1,163 @@
+/* Based on code from the 2024 master's thesis "Development of an Autonomous Rabbit for Running on a Track"
+ * by Kvamme et al. at NTNU, Norway.
+ *
+ * Communication with the ZED Box over Ethernet.
+ * Connects to the ZED Box as a TCP client and receives setpoints and commands for the car's:
+ *     - Electronic Speed Controller (ESC)
+ *     - Steering servo
+ *     - Braking servo
+ *     - Rear LED lights (green and red)
+ * 
+ * Incoming message structure - 32 bytes:
+ *     - Byte 0.0:    Run signal          (bool)
+ *     - Byte 1.0:    Emergency brake     (bool)
+ *     - Byte 2.0:    Green LED           (bool)
+ *     - Byte 3.0:    Red LED             (bool)
+ *     - Byte 4-15:   Spare             
+ *     - Byte 16-19:  Message nr          (uint32_t)    (Increments per message. Can be used as watchdog / lost message detector.)
+ *     - Byte 20-23:  Steer angle         (float)
+ *     - Byte 24-27:  Desired speed PWM   (float)       (Pulse width in microseconds)
+ *     - Byte 28-31:  Spare
+ * 
+ * Outgoing message structure - 32 bytes:
+ *     - Byte 0.0:    Run signal          (bool)        (Mirror incoming message)
+ *     - Byte 1.0:    Emergency brake     (bool)        (Mirror incoming message)
+ *     - Byte 2.0:    Green LED           (bool)        (Read pin value)
+ *     - Byte 3.0:    Red LED             (bool)        (Read pin value)
+ *     - Byte 6-15:   Spare
+ *     - Byte 16-19:  Message nr          (uint32_t)    (Increments per message)
+ *     - Byte 20-23:  ESC min PWM         (float)       (ESC calibrated min PWM limit - zero point)
+ *     - Byte 24-27:  ESC max PWM         (float)       (ESC calibrated max PWM limit)
+ *     - Byte 28-31:  PWM max speed limit (float)       (Hardcoded PWM limit for safety)
+ * 
+ * Notes:
+ *     - Programmed for Arduino Uno (8-bit) with Arduino Ethernet Shield 2 (W5500 chip).
+ *         - Shield supports 8 independent sockets. A socket's internal RX and TX memory buffer is 2KB each.
+ *         - Float data type is truncated and not rounded when assigned to an int. (Angle of 90.9 becomes 90, not 91.)
+ *         - Not tested on 32-bit boards, but data types are chosen to be compatible.
+ *     - Servo library interprets values below 200 as angles, and values above 200 as microsecond pulse widths (PWM).
+ *     - PWM values are gained from testing. A new ESC may need to be calibrated anew by teaching max/min values.
+*/
+// ============================================
+// PREAMBLE (Includes and definitions)
+// ============================================
+
 #include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Servo.h>
 
-#define BRAKE_ANGLE 120
-#define NO_BRAKE_ANGLE 87
 #define RED_LED_PIN 2
 #define GREEN_LED_PIN 7
 #define STEER_PWM_PIN 5
 #define BRAKE_PWM_PIN 6
 #define ESC_PWM_PIN 3
 #define REGULATOR_PIN 8
-#define BUFFER_SIZE 11
-#define NO_SPEED 1500
-#define MIN_SPEED 1540
-#define MAX_SPEED 2000
-#define SPEED_LIMIT 1650 // Artificial limit for safety
-#define TIMEOUT 3000     // If incoming message takes more time than this (ms) the car will deactivate
+
+// Incoming TCP message offsets
+#define OFFSET_RUN_SIGNAL 0
+#define OFFSET_EMERGENCY_BRAKE 1
+#define OFFSET_GREEN_LED 2
+#define OFFSET_RED_LED 3
+#define OFFSET_MESSAGE_NR 16
+#define OFFSET_STEER_ANGLE 20
+#define OFFSET_DESIRED_SPEED_PWM 24
+
+// Outgoing TCP message offsets
+#define OFFSET_OUT_RUN_SIGNAL 0
+#define OFFSET_OUT_EMERGENCY_BRAKE 1
+#define OFFSET_OUT_GREEN_LED 2
+#define OFFSET_OUT_RED_LED 3
+#define OFFSET_OUT_MESSAGE_NR 16
+#define OFFSET_OUT_ESC_MIN_PWM 20
+#define OFFSET_OUT_ESC_MAX_PWM 24
+#define OFFSET_OUT_PWM_SPEED_LIMIT 28
+
+// ESC PWM pulse widths in microseconds
+#define SPEED_PWM_STOP 1500   // Zero point PWM when tuning ESC - Stands still around 1500-1599
+#define SPEED_PWM_MIN 1540    
+#define SPEED_PWM_MAX 2000    // Max PWM when tuning ESC - Roughly 22.7 m/s or 80 km/h.
+#define SPEED_PWM_LIMIT 1650  // Artificial limit for safety - Roughly 5.4 m/s or 19.6 km/h
+
+// Steering servo angles (note: values below 200 are treated as angles by servo library)
+#define STEER_ANGLE_NEUTRAL 90
+#define STEER_ANGLE_MAX_RIGHT 45
+#define STEER_ANGLE_MAX_LEFT 135
+#define STEER_PWM_MIN 863
+#define STEER_PWM_MAX 2063
+
+// Braking servo angles (note: values below 200 are treated as angles by servo library)
+#define BRAKE_ANGLE_ON 120
+#define BRAKE_ANGLE_OFF 87
+#define BRAKE_PWM_MIN 650
+#define BRAKE_PWM_MAX 2250
+
+// Network parameters
+#define INPUT_BUFFER_SIZE 32        // Byte size of TCP message from ZED Box
+#define OUTPUT_BUFFER_SIZE 32       // Byte size of TCP message to ZED Box
+#define TIMEOUT 1000                // If incoming message takes more time than this (ms) the car will deactivate
+#define ZEDBOX_PORT 49152           // TCP port on ZED Box - 49152 and up are unused
+#define ZEDBOX_IP 192, 168, 56, 1
+#define ARDUINO_IP 192, 168, 56, 2
+#define ARDUINO_MAC 0xA8, 0x61, 0x0A, 0xAF, 0x19, 0x43 // Mac address of arduino shield, read from sticker on shield
 
 
-// The MAC-address of the arduino Shield (Written on a sticker on the shield)
-byte mac[] = {0xA8, 0x61, 0x0A, 0xAF, 0x19, 0x43};
+// ============================================
+// VARIABLE AND OBJECT DEFINITIONS
+// ============================================
 
-// The IP-address of the computer (MÅ ENDRES TIL ZED BOX)
-IPAddress server_ip(192, 168, 56, 1);
-
-// Set the IP-address of the arduino (The 3 first sections must be identical to the IP-address of the computer)
-IPAddress ip(192, 168, 56, 2);
-
-// Port numbers from 49152 and up are guaranteed to be unused
-unsigned int server_port = 49152;
-
+// Network setup
 EthernetClient client;
+IPAddress ip(ARDUINO_IP);
+byte mac[] = {ARDUINO_MAC};
+IPAddress server_ip(ZEDBOX_IP);
+uint16_t server_port = ZEDBOX_PORT;
 
-// Variables to store received data
+// ESC and servos
 Servo Steer; // 1199 = max right, 1200-1499 = right, 1500-1599 = 0 degrees, 1600-1899 = left, 1900 = max left
 Servo Brake;
 Servo ESC;  // 1500-1599: stationary, 1600 = min speed, 2000 = max speed
+int16_t ESCpwm = SPEED_PWM_STOP;
 
-float desiredSpeed;
 
-bool greenBool;
-float steerAngle = 90; // 90 = straight, 45 = max right, 135 = max left
-int ESCpwm = NO_SPEED;
+// Variables for received data
+byte buffer_in[INPUT_BUFFER_SIZE];
+bool runSignal;
+bool emergencyBrake;
+bool greenLED;
+bool redLED;
+uint32_t msgNr_in = 0;
+// float desiredSpeed; // Legacy code: ESCpwm = desiredSpeed * 20.23 + MIN_SPEED
+float desiredSpeed_pwm = SPEED_PWM_STOP;
+float steerAngle = STEER_ANGLE_NEUTRAL;
 
+// Variables for outgoing data
+byte buffer_out[OUTPUT_BUFFER_SIZE];
+uint32_t msgNr_out = 0;
+float escMinPWM = SPEED_PWM_MIN;
+float escMaxPWM = SPEED_PWM_MAX;
+float pwmSpeedLimit = SPEED_PWM_LIMIT;
+
+// Internal logic
 
 bool idle = true;
-bool runFinished = false;
-
-unsigned long timeStamp; 
 unsigned long watchdogTimer;
 
 
-void setOutputs(byte buffer[BUFFER_SIZE]);     // Set vehicle parameters
-//void printBuffer(byte buffer[11]);          // Print received values
+// ============================================
+// FORWARD FUNCTION DECLARATIONS - defined after loop())
+// ============================================
+
+void recvData(byte buffer[INPUT_BUFFER_SIZE]);      // Read incoming message buffer and store data in variables
+void setOutputs();                                  // Set outputs according to variables updated by rcvData()
+void sendData();                                    // Send feedback message to ZED Box
+
+
+// ============================================
+// SETUP AND LOOP
+// ============================================
 
 void setup() {
-  //pinMode(ESC_PWM_PIN, OUTPUT);
-  //digitalWrite(ESC_PWM_PIN, LOW);
   ESC.attach(ESC_PWM_PIN);
   ESC.writeMicroseconds(ESCpwm);
   pinMode(RED_LED_PIN, OUTPUT);
@@ -65,13 +165,12 @@ void setup() {
   digitalWrite(RED_LED_PIN, LOW);
   digitalWrite(GREEN_LED_PIN, LOW);
   
-  Brake.attach(BRAKE_PWM_PIN, 650, 2250);
-  Brake.write(NO_BRAKE_ANGLE);
+  Brake.attach(BRAKE_PWM_PIN, BRAKE_PWM_MIN, BRAKE_PWM_MAX);
+  Brake.write(BRAKE_ANGLE_OFF);
 
-  Steer.attach(STEER_PWM_PIN,  863, 2063);
-  Steer.write(steerAngle);
+  Steer.attach(STEER_PWM_PIN,  STEER_PWM_MIN, STEER_PWM_MAX);
+  Steer.write(STEER_ANGLE_NEUTRAL);
   
-
   // Disable SD Card (Not sure if necessary, 
   // but read in an arduino forum that it could cause issues with ethernet comms)
   // arduino docs also say that pins 4, 11, 12 and 13 cannot be used for digital output because of ethernet
@@ -82,7 +181,6 @@ void setup() {
   pinMode(REGULATOR_PIN, OUTPUT);
   digitalWrite(REGULATOR_PIN, HIGH);
 
-  
   // Start Ethernet connection
   Ethernet.begin(mac, ip); 
   delay(5000);
@@ -92,110 +190,93 @@ void setup() {
   digitalWrite(RED_LED_PIN, HIGH);
   digitalWrite(GREEN_LED_PIN, HIGH);
 
-
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if ( ((millis() - watchdogTimer) > TIMEOUT) && !idle){ //In case communication is broken
+  // Stop if no message is received for a certain time
+  if ( ((millis() - watchdogTimer) > TIMEOUT) && !idle){
       idle = true;
-      ESCpwm = NO_SPEED;
+      ESCpwm = SPEED_PWM_STOP;
       ESC.writeMicroseconds(ESCpwm);
-      Brake.write(BRAKE_ANGLE);
+      Brake.write(BRAKE_ANGLE_ON);
       digitalWrite(RED_LED_PIN, HIGH);
       digitalWrite(GREEN_LED_PIN, LOW);
-      runFinished = true;
-    }
-
-
-  //Receive angle
-  if (client.connected()) {
-    
-
-    if (client.available() >= BUFFER_SIZE) {
-      watchdogTimer = millis(); // Reset watchdog timer
-
-      // Read data into buffer
-      byte buffer[BUFFER_SIZE];
-      client.read(buffer, BUFFER_SIZE);
-
-      if (!runFinished) setOutputs(buffer); // Set vehicle parameters
-      //printBuffer(buffer); // Print values, comment out in practical application
-    }
   }
-  else {
-    client.connect(server_ip, server_port);
+
+  // Normal operation: Receive data -> set outputs accordingly -> send feedback message
+  if (client.connected()) {
+    if (client.available() >= INPUT_BUFFER_SIZE) {
+      watchdogTimer = millis(); // Reset timer
+      idle = false;
+
+      recvData(buffer_in); // Read message from ZED Box and updates variables
+
+      setOutputs(); // Set outputs based on the updated variables from ZED Box
+
+      sendData(); // Send feedback message back to ZED Box
+    }
+  } else {
+      client.connect(server_ip, server_port); // Auto-reconnect
   }
 
 }
 
-void setOutputs(byte buffer[BUFFER_SIZE]) {
+
+// ============================================
+// HELPER FUNCTION DEFINITIONS
+// ============================================
+
+// Read incoming message from ZED Box and store in buffer
+void recvData(byte buffer[INPUT_BUFFER_SIZE]) {
+  client.read(buffer, INPUT_BUFFER_SIZE);
+  // Extract data from buffer to respective variables
+  runSignal = buffer[OFFSET_RUN_SIGNAL];
+  emergencyBrake = buffer[OFFSET_EMERGENCY_BRAKE];
+  greenLED = buffer[OFFSET_GREEN_LED];
+  redLED = buffer[OFFSET_RED_LED];
+  memcpy(&msgNr_in, buffer + OFFSET_MESSAGE_NR, sizeof(msgNr_in));
+  memcpy(&steerAngle, buffer + OFFSET_STEER_ANGLE, sizeof(steerAngle));
+  memcpy(&desiredSpeed_pwm, buffer + OFFSET_DESIRED_SPEED_PWM, sizeof(desiredSpeed_pwm));
+}
 
 
-//sig,brake,angle,speed
-  
-  memcpy(&steerAngle, buffer + 3, sizeof(steerAngle));
-  memcpy(&desiredSpeed, buffer + 7, sizeof(desiredSpeed));
-  
-  //Serial.println(desiredSpeed);
-  
+// Set outputs based on received data from ZED Box
+void setOutputs() {
+  // LEDs
+  digitalWrite(GREEN_LED_PIN, greenLED);
+  digitalWrite(RED_LED_PIN, redLED);
 
-
-  if (buffer[0] == true) { // On signal
-
-    if (buffer[1] == true) { //Emergency brake
-      ESCpwm = NO_SPEED;
+  if (!runSignal || emergencyBrake) { // Brake
+      ESCpwm = SPEED_PWM_STOP;
       ESC.writeMicroseconds(ESCpwm);
-      Brake.write(BRAKE_ANGLE);
-    }
+      Brake.write(BRAKE_ANGLE_ON);
 
-    else{
-      //constrain angle
-      steerAngle = constrain(steerAngle, 45, 135);
+  } else{ // Normal operation: let ZED Box control the car
+      steerAngle = constrain(steerAngle, STEER_ANGLE_MAX_RIGHT, STEER_ANGLE_MAX_LEFT);
       Steer.write(steerAngle);
 
-      digitalWrite(RED_LED_PIN, LOW);
-
-      ESCpwm = static_cast<int>(desiredSpeed * 20.23 + MIN_SPEED); // found through testing, PID output desiredSpeed correspons roughly to m/s
-
-      if (ESCpwm <= MIN_SPEED) { // Shouldn't be a possible case
-        ESCpwm = NO_SPEED;
-      }
-
-      else if (ESCpwm > SPEED_LIMIT) { // Desired speed is higher than allowed
-        ESCpwm = SPEED_LIMIT;
-        digitalWrite(RED_LED_PIN, HIGH);
-
-      }
-
-      else { // Valid speed
-        digitalWrite(RED_LED_PIN, LOW);
-      }
-
-      greenBool = buffer[2];
-      digitalWrite(GREEN_LED_PIN, greenBool);
-      
-      timeStamp = millis(); // Start timer
-      //startRun = true;
-      idle = false;
-      //startRun = false;
-      timeStamp = millis();
+      ESCpwm = static_cast<int>(desiredSpeed_pwm);
+      if (ESCpwm <= SPEED_PWM_MIN) {ESCpwm = SPEED_PWM_STOP;}
+      else if (ESCpwm > SPEED_PWM_LIMIT) {ESCpwm = SPEED_PWM_LIMIT;}
       ESC.writeMicroseconds(ESCpwm);
-      
-    }
-
-
   }
-  else { // ZED Box has sent deactivation message
-    
-    ESCpwm = NO_SPEED;
-    ESC.writeMicroseconds(ESCpwm);
-    Brake.write(BRAKE_ANGLE);
-    idle = true;
-    runFinished = true;
-    digitalWrite(GREEN_LED_PIN, LOW);
-    digitalWrite(RED_LED_PIN, LOW);
-  }
+}
 
+
+// Send feedback to ZED Box
+void sendData() {
+  buffer_out[OFFSET_OUT_RUN_SIGNAL] = runSignal;
+  buffer_out[OFFSET_OUT_EMERGENCY_BRAKE] = emergencyBrake;
+  buffer_out[OFFSET_OUT_GREEN_LED] = digitalRead(GREEN_LED_PIN);
+  buffer_out[OFFSET_OUT_RED_LED] = digitalRead(RED_LED_PIN);
+  memcpy(buffer_out + OFFSET_OUT_MESSAGE_NR, &msgNr_out, sizeof(msgNr_out));
+  memcpy(buffer_out + OFFSET_OUT_ESC_MIN_PWM, &escMinPWM, sizeof(escMinPWM));
+  memcpy(buffer_out + OFFSET_OUT_ESC_MAX_PWM, &escMaxPWM, sizeof(escMaxPWM));
+  memcpy(buffer_out + OFFSET_OUT_PWM_SPEED_LIMIT, &pwmSpeedLimit, sizeof(pwmSpeedLimit));
+
+  client.write(buffer_out, OUTPUT_BUFFER_SIZE);
+  msgNr_out++;
 }
