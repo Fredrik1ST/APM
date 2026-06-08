bb9abcd219c4d8cf405fa8bc3cb1b009daf93984
Fredrik Taklo <fredriktaklo@hotmail.com>
2026-06-08 19:22:49 +0200

    Add tests/input folder for offline testing

--- conftest.py
+++ conftest.py
@@ -7,11 +7,19 @@ import os
 sys.path.insert(0, os.path.dirname(__file__))
 
 
-# --- Create default output directory for figures etc. ---
+# --- Create default input / output directory for test data (images, figures, etc.) ---
 from pathlib import Path
 import pytest
+
+INPUT_DIR = Path(__file__).resolve().parent / 'tests' / 'input'
+INPUT_DIR.mkdir(exist_ok=True)
+
 OUTPUT_DIR = Path(__file__).resolve().parent / 'tests' / 'output'
 OUTPUT_DIR.mkdir(exist_ok=True)
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_input_dir():
+    return INPUT_DIR
 
 @pytest.fixture(scope='session', autouse=True)
 def setup_output_dir():
